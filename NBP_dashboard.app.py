494259f455d1ee451cae57306441982943f3dfa1
dbanyaibecker <159194182+dbanyaibecker@users.noreply.github.com>
2024-09-03 01:01:16 +0000

    same as last

--- NBP_dashboard.app.py
+++ NBP_dashboard.app.py
@@ -239,7 +239,7 @@ with col1:
                      tickfont = dict(size = 18, color = 'black')) 
     theme_bcs(fig1)
     st.write(fig1)
-    st.markdown("<div style = 'height: 500px;'></div>", unsafe_allow_html=True)
+    st.markdown("<div style = 'height: 400px;'></div>", unsafe_allow_html=True)
     st.image(image2, width=700)
 
 with gap: 
